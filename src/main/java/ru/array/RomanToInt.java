c046bc992d47d15b638e4d7d236dc47c85021202
shaporen <shaporen@yandex.ru>
2024-03-30 01:02:37 +0300

    Roman To Int with HashMap

--- src/main/java/ru/array/RomanToInt.java
+++ src/main/java/ru/array/RomanToInt.java
@@ -1,23 +1,31 @@
 package ru.array;
 
+import java.util.*;
+
 /**
  * Переводит римские цифры в арабские
  */
 public class RomanToInt {
     public static int romanToInt(String s) {
-        int ans = 0, num = 0;
+        int ans = 0;
+        int num = 0;
+        Map<String, Integer> map = new HashMap<>(7);
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put("M", 1000);
         for (int i = s.length() - 1; i >= 0; i--) {
-            switch (s.charAt(i)) {
-                case 'I' -> num = 1;
-                case 'V' -> num = 5;
-                case 'X' -> num = 10;
-                case 'L' -> num = 50;
-                case 'C' -> num = 100;
-                case 'D' -> num = 500;
-                case 'M' -> num = 1000;
+            if (map.containsKey(String.valueOf(s.charAt(i)))) {
+                num = map.get(String.valueOf(s.charAt(i)));
+                if (4 * num < ans) {
+                    ans -= num;
+                } else {
+                    ans += num;
+                }
             }
-            if (4 * num < ans) ans -= num;
-            else ans += num;
         }
         return ans;
     }
