63af09f5e12758d13e9aebeb90330bfa913bcc65
shaporen <shaporen@yandex.ru>
2024-03-29 23:52:24 +0300

    Merge Two Sorted Lists (different lengths)

--- src/main/java/ru/algoritms/MergeTwoSortedLists.java
+++ src/main/java/ru/algoritms/MergeTwoSortedLists.java
@@ -5,21 +5,15 @@ import java.util.*;
 public class MergeTwoSortedLists {
     public static List<Integer> mergeTwoLists(List<Integer> list1, List<Integer> list2) {
         List<Integer> list = new ArrayList<>(list1.size() + list2.size());
-        for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i) >= list2.get(i)) {
-                list.add(list2.get(i));
-                list.add(list1.get(i));
-            } else {
-                list.add(list1.get(i));
-                list.add(list2.get(i));
-            }
-        }
+        list.addAll(list1);
+        list.addAll(list2);
+        list.sort(Comparator.naturalOrder());
         return list;
     }
 
     public static void main(String[] args) {
         List<Integer> list1 = List.of(1, 2, 7);
-        List<Integer> list2 = List.of(2, 4, 5);
+        List<Integer> list2 = List.of(2, 4, 5, 7);
         List<Integer> list = mergeTwoLists(list1, list2);
         list.stream().forEach(System.out::println);
     }
