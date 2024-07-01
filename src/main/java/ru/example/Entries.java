1bf6ea43d5631dfea621a334db55eeb012f89da8
shaporen <shaporen@yandex.ru>
2024-07-01 23:21:11 +0300

    entries

---
+++ src/main/java/ru/example/Entries.java
@@ -0,0 +1,28 @@
+package ru.example;
+
+import java.util.*;
+
+public class Entries {
+    public static void main(String[] args) {
+        Map<String, Integer> ages = new HashMap<>();
+        ages.put("Alice", 25);
+        ages.put("Bob", 30);
+        ages.put("Charlie", 28);
+
+        Set<String> keys = ages.keySet();
+        System.out.println("Keys: " + keys);
+
+        for (String key : keys) {
+            System.out.println(key);
+        }
+
+        Set<Map.Entry<String, Integer>> entries = ages.entrySet();
+        System.out.println("Entries: " + entries);
+
+        for (Map.Entry<String, Integer> entry : entries) {
+            System.out.println("Key: " + entry.getKey() + ". Value: " + entry.getValue());
+        }
+
+        System.out.println(ages.values());
+    }
+}
